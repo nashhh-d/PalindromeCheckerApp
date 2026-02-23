4d6ccb2d6a762f4e9ee2dcc40aae8e0630f5acfa
nashhh-d <nashud.06@gmail.com>
2026-02-23 14:22:05 +0530

    base setip

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("======================================");
+        System.out.println("      Welcome to Palindrome Checker App");
+        System.out.println("======================================");
+
+        // Application Details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("Description      : Validates whether a given string is a palindrome.");
+
+        System.out.println("--------------------------------------");
+        System.out.println("Application started successfully!");
+        System.out.println("--------------------------------------");
+    }
+
+}
